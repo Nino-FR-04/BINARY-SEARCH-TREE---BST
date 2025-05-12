c5205d626121b8c3e8b05d75cc9dee2e1a8a1de8
Nino-FR-04 <davidfrx322@gmail.com>
2025-05-11 19:32:03 -0500

    Interfaz TAD para la implementación de un BST

---
+++ impl_Flores_Nino/BinarySearchTree/TADBinarySearchTree.java
@@ -0,0 +1,15 @@
+package impl_Flores_Nino.BinarySearchTree;
+
+import impl_Flores_Nino.Excepciones.*;
+
+/**
+ * Interfaz que define las operaciones básicas de un BST genérica.
+ * 
+ * @param E el tipo de elementos que almacena el BST.
+ */
+public interface TADBinarySearchTree <E> {
+    public void insert(E data) throws ExceptionItemDuplicated;
+    public E search(E data) throws ExceptionItemNotFound;
+    public void delete(E data) throws ExceptionIsEmpty;
+    public boolean isEmpty();
+}
