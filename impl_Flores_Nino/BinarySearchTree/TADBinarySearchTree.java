c24886abb53f2cb8865276056c0ad7b73860d4db
Nino-FR-04 <davidfrx322@gmail.com>
2025-05-13 11:12:57 -0500

    Actualizacion TAD BST

--- impl_Flores_Nino/BinarySearchTree/TADBinarySearchTree.java
+++ impl_Flores_Nino/BinarySearchTree/TADBinarySearchTree.java
@@ -12,4 +12,8 @@ public interface TADBinarySearchTree <E> {
     public E search(E data) throws ExceptionItemNotFound;
     public void delete(E data) throws ExceptionIsEmpty;
     public boolean isEmpty();
+    public void destroyNodes() throws ExceptionIsEmpty;
+    public int countAllNodes(); //Nodos no hojas
+    public int height(E subRoot);
+    public int amplitude(E subRoot);
 }
